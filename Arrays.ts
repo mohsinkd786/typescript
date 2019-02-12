f9063a2ee470c210f863511c58881a3f44ad7d9a
Mohsin K <mohsinkd786@gmail.com>
2019-02-12 12:29:13 +0530

    Arrays

---
+++ Arrays.ts
@@ -0,0 +1,25 @@
+let arr:number[]
+
+arr = []
+
+arr[0] = 10
+arr[1] = 2
+
+console.log(`Length of Array : ${arr.length}`)
+console.log(arr[0])
+console.log(arr[1])
+
+let numbers:number[] = [1,10,2,8]
+let vals =[
+    {
+        id:1
+    },{
+        id:2
+    }
+]
+console.log(numbers[1])
+
+let index:number
+for(index=0;index<numbers.length;index++){
+    console.log('Number is ',numbers[index])
+}
