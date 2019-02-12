81d8aa411ed23d7b47f31b781a41d7651c3fc7d7
Mohsin K <mohsinkd786@gmail.com>
2019-02-12 16:53:50 +0530

    Multiple Inheritance & Method declarations

--- Employee.ts
+++ Employee.ts
@@ -1,18 +1,53 @@
-interface Human{
+interface User {
     id: number,
     name: string,
     email: string
 }
-
-// address
-interface Address{
-    street: string,
+interface Address {
+    street : string,
     city: string,
     state: string,
-    zipcode: number
+    zip: number
 }
-interface Employee extends Human,Address{
+interface UnEmployed extends User{
+    //salary:number
+}
+interface Employed extends User,Address{
+    salary : number,
+    getNameAndEmail:()=>string
+}
+// actual body with methods implementations
+let emp:Employed ={
+    id: 1,
+    name: 'Bob Marley',
+    email: 'bob@marley.com',
+    street: 'Chowpati',
+    city: 'Mumbai',
+    state: 'Maharashtra',
+    zip: 20001,
+    salary: 500000,
+    getNameAndEmail:()=>{
+        return emp.name +' '+emp.email
+    }
 }
 
-//getUser:()=>string
+// child to parent 
+let _parent:User = {
+    id: 1,
+    name: 'Mohsin',
+    email: 'mo@gmail.com'
+}
 
+let _child:UnEmployed = _parent
+
+// parent to child
+let __child:UnEmployed = {
+    id: 1,
+    name: 'Mohsin',
+    email: 'mo@gmail.com'
+   //,salary: 10
+}
+
+let __parent:User = __child
+
+console.log(emp.getNameAndEmail())
