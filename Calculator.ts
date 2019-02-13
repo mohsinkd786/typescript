1f1aad776a4feb5e98ac7b47d71b2d994221c06a
Mohsin K <mohsinkd786@gmail.com>
2019-02-13 15:58:10 +0530

    Code cleaning

--- Calculator.ts
+++ Calculator.ts
@@ -3,6 +3,12 @@ import sub = require('./Sub')
 import { Multiply } from './Multiply'
 import { IOperation } from "./Operation"
 
+class ACTIONS{
+    static _SUM:any = new add.Operation.Addition()
+    static _DIFF:any = new sub.Proc.Subtract()
+    static _PRODUCT:any = new Multiply()
+}
+
 const _process = (_first:number,_next:number,_action:IOperation)=>{
     return _action._operate(_first,_next)
 }
@@ -13,17 +19,17 @@ const _calculate = (_first:number,_next:number,_action:string)=>{
     switch(_action){
         case 'SUM':
             // #### ADDITION
-            const _addObj = new add.Operation.Addition()
+            const _addObj = ACTIONS._SUM
             console.log(`${_action} IS ${_process(_first,_next,_addObj)}`)
             break
         case 'DIFF':
             // #### DIFFERENCE
-            const _diffObj = new sub.Proc.Subtract()
+            const _diffObj = ACTIONS._DIFF
             console.log(`${_action} IS ${_process(_first,_next,_diffObj)}`)
             break
         case 'PRODUCT':
             // #### PRODUCT
-            const _mulObj = new Multiply()
+            const _mulObj =ACTIONS._PRODUCT
             console.log(`${_action} IS ${_process(_first,_next,_mulObj)}`)
             break
         default:
